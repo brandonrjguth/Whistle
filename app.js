db42a66f5bf23458623644109900b674d5511387
brandonrjguth <38782671+brandonrjguth@users.noreply.github.com>
2020-07-01 14:09:12 -0700

    Webpage launched by express server using Node. Initial integration of Socket.IO

---
+++ app.js
@@ -0,0 +1,55 @@
+//INITIAL SETUP
+
+
+    //Requires (Body Parser, EJS, Express, Mongo, Mongoose)
+    const bodyParser = require('body-parser');
+    const ejs = require("ejs");
+    const express = require("express");
+    const { MongoClient } = require("mongodb");
+    const mongoose = require('mongoose');
+    const app = express();
+    const http = require('http').createServer(app);
+    const io = require('socket.io')(http);
+
+//SET EXPRESS AND MONGO CONNECTION PORT AND URL.
+
+    //Set port express will listen on.
+    const port = 3000;
+
+//OTHER
+
+    //Configure Body Parser and EJS
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.set('view engine', 'ejs');
+    app.use(express.static(__dirname + '/public'));
+
+
+
+//ROUTES
+
+app.get('/', function(req, res) {
+    res.render('streamVideo');
+});
+
+app.post('/testButton', function(req, res) {
+    console.log(req.body.input);
+
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+    });
+});
+
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+    
+  });
